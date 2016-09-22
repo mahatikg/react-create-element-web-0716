dc48b7382ab79d868420645910f9862db8e317b7
Mahati Gollamudi <github email address>
2016-09-22 19:05:41 -0400

    Create Element

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const list =
     React.createElement('h1', {}, 'My favorite ice cream flavors'),
     React.createElement('ul', {},
       [
-        React.createElement('li', { class: 'brown' }, 'Chocolate'),
-        React.createElement('li', { class: 'white' }, 'Vanilla'),
-        React.createElement('li', { class: 'yellow' }, 'Banana')
+        React.createElement('li', {}, 'Chocolate'),
+        React.createElement('li', {}, 'Vanilla'),
+        React.createElement('li', {}, 'Banana')
       ]
-    ));
+    );
+
+  const title = React.createElement('h1', {}, 'My First React Code');
+  const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
+  const container = React.createElement('div', {}, [title, paragraph]);
+
+
 
 ReactDOM.render(list, document.getElementById('main'));
+
+ReactDOM.render(
+  container,
+  document.getElementById('main')
+);
